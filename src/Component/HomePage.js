47f90810506f8bd0ac2566c9640ae7741d8079c7
mohdsadique012 <mohdsadique012000@gmail.com>
2023-08-22 18:52:41 +0530

    1st

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -1,71 +1,59 @@
-import React, { useEffect, useState} from 'react'
-
-import "./Homepage.css"
+import React, { useEffect, useState } from 'react';
+import "./Homepage.css";
 
 function HomePage() {
-    useEffect(()=>{
+    useEffect(() => {
         fetch("https://fakestoreapi.com/products")
-        .then(res=>res.json())
-        .then(data=>{
-         setData(data)
-         setFilter(data)
-         
-        })
-         
-     },[])  
-    const [data,setData]=useState([])
-    const [filter,setFilter]=useState([])
-    const allProduct= filter
-    console.log(allProduct,"kkk")
-    const key = "category"
-    const category = [... new Map(data.map(item=> [item[key], item])).values()];
-     console.log(category,"jjj")
+            .then(res => res.json())
+            .then(data => {
+                setData(data);
+                setFilter(data);
+            });
+    }, []);
 
+    const [data, setData] = useState([]);
+    const [filter, setFilter] = useState([]);
+    const allProduct = filter;
+    const categoryKey = "category";
+    const categories = [...new Map(data.map(item => [item[categoryKey], item])).values()];
 
+    const handleClick = (category) => {
+        const filteredProducts = data.filter(item => item[categoryKey] === category);
+        setFilter(filteredProducts);
+    };
 
+    const handleAllProductsClick = () => {
+        setFilter(data);
+    };
 
+    return (
+        <>
+            <div className='main_container'>
+                <div className='category-container'>
+                    <button className='button' onClick={handleAllProductsClick}>All Products</button>
+                    {categories.map((category) => (
+                        <button onClick={() => handleClick(category[categoryKey])} className='button' key={category[categoryKey]}>
+                            {category[categoryKey]}
+                        </button>
+                    ))}
+                </div>
 
-const handleClick=(e)=>{
-   const filterProduct=data.filter((item)=>{
-        return item.category === e
-    })
-    setFilter(filterProduct)
-}
-const handleclickitem=()=>{
-    setFilter(data)
-}
-  return (
-<>
- <div className='main_container'>
-
-
- 
-<div className='category-container' > 
-<button className='button' onClick={()=>handleclickitem()}>allProduct</button>
-{category.map((category)=>{
-    return <button  onClick={()=>handleClick(category.category)} className='button'>{category.category}</button>
-})}
-</div>
-
- { allProduct.map((item)=>(
-    <div className='Content_box'>
-    <img src={item.image} className='image'/>
-    <div>
-      <p>{item.category}</p>
-      <h6>{item.title} </h6>
-      <span> <h>price:{item.price} </h><h>Rating:{item.rating.rate}</h></span>
-      <p></p>
-    </div>
-    
-  </div>
- )
-  
-
- )}
-    
- </div>
-</>
-  )
+                {allProduct.map((item) => (
+                    <div className='Content_box' key={item.id}>
+                        <img src={item.image} className='image' alt={item.title} />
+                        <div>
+                            <p>{item[categoryKey]}</p>
+                            <h6>{item.title}</h6>
+                            <span>
+                                <span>Price: {item.price}</span> <span>Rating: {item.rating.rate}</span>
+                            </span>
+                            <p></p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
 
-export default HomePage
+export default HomePage;
